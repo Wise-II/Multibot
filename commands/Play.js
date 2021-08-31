91e814f831f9c586acb5fb17595d267098673d46
Wise-XXIV <81892372+Wise-XXIV@users.noreply.github.com>
2021-08-31 14:35:30 -0400

    Update Play.js
    added multiple play calls

--- commands/Play.js
+++ commands/Play.js
@@ -3,6 +3,7 @@ const ytdl = require("ytdl-core");
 module.exports = {
   name: "play",
   description: "Play a song in your channel!",
+  usuge: 'm?play m?',
   async execute(message) {
     try {
       const args = message.content.split(" ");
