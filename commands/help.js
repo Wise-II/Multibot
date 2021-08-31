6b6083e0a4625175cb07ab83b86a41e29fe66847
Wise-XXIV <81892372+Wise-XXIV@users.noreply.github.com>
2021-08-31 12:57:49 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,9 @@
+const { dir } = require("console");
 const { MessageEmbed } = require("discord.js");
 const { readdirSync } = require("fs");
 
 module.exports = {
-  name: ''
+  name: 'help',
+  description: 'informs the user of the dynahelp',
+  usage: 'm?help'
 }
