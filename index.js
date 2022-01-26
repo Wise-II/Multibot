4533b73651e58d7a4303ddbdd7cdb18ff71dfbc9
Wise_XXIV <dz.diego5967@outlook.com>
2022-01-26 11:56:37 -0500

    added activities List and Bash script to start bot

--- index.js
+++ index.js
@@ -46,12 +46,24 @@ client.on("message", (message) => {
     cmdhandler.handleCommand(message)
 })
 const activities_list = [
-  "online...", 
+  "Online...Why do i exist", 
+  "High....",
+  "BEEP BEEP SKEBOP",
+  "50,000$ later,",
+  "Th33Kha0s eating a BeastBurger is the funniest thing i saw",
+  "Deez Nutz Innit",
+  "NYOOOOOOOOOOOOOOOM",
+  "AAAAAAAAAAAAAH A SPIDERRRRRR!",
+  "i don't understand how a woman works -mario_time 2021",
+  "sudo delete your existence.exe",
+  "shush, you dont know shet"
+  
+
   ]; // creates an arraylist containing phrases you want your bot to switch through.
 
 client.on('ready', () => {
   
-      client.user.setActivity('Help command still doesnt work :( | M?'); 
+      client.user.setActivity(activities_list) 
 });
 client.on('message', message => {
 if (message.mentions.has(client.user)) {
@@ -70,7 +82,7 @@ client.on('messageDelete', message => {
   .addField('in channel', message.channel)
   .addField('User', `${message.author}`)
   .addField('Content in message', "`" + "``asciidoc\n" + `[${message.content}]` + "\n`" + "``")
-  .setFooter('MultiBot -> Loggging, Deleted')
+  .setFooter('MiataBot -> Loggging, Deleted Messages')
   .setTimestamp()
   .setColor('#8B0000')
   .setThumbnail(user.displayAvatarURL({ dynamic:true }))
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 echo -e "\e[1;31m Starting bot \e[0m"
 :main
 node index.js
+node main.js
 echo echo -e "\e[1;31m restarting bot \e[0m"
